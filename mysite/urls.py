4c80b6fa9e5fb8c749a92e1f9fa58350d53119ee
budebajohn003-max <budebajohn003@gmail.com>
2026-08-30 19:11:48 +0300

    Implement fallback for system_demo import
    Added error handling for importing system_demo view.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,31 @@
 """
-URL configuration wrapper for mysite project.
-Imports from inner URLs module and applies path corrections.
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from service.views import system_demo
+
+try:
+    from service.views import system_demo
+except ImportError:
+    # Define a fallback view if service.views can't be imported during build
+    def system_demo(request):
+        from django.http import HttpResponse
+        return HttpResponse("System demo not available during build phase")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
